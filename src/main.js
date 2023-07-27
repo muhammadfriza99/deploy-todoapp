44126eb81523f7da64575c8d4562f781e7ec52c8
frizamuhammad99 <frizamuhammad99@gmail.com>
2023-07-27 11:28:56 +0700

    smail bug

--- src/main.js
+++ src/main.js
@@ -5,7 +5,8 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
+app.config.productionTip = false
+app.config.globalProperties.$base = '/deploy-todoapp/'
 app.use(router)
 
 app.mount('#app')
